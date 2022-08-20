c5638dd4d464c151b3fb3a3f5f43d57821319cd0
wanosukecake <wanosukecake@gmail.com>
2022-08-20 13:02:01 +0700

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 本番環境(Heroku)でhttpsを強制する
+        // if (\App::environment('production')) {
+        //     \URL::forceScheme('https');
+        // }
+        Schema::defaultStringLength(191);
     }
 }
