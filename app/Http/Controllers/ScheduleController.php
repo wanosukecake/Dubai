625b8a350005fc79e3f040fe632016e79dc0ecfe
wanosukecake <wanosukecake@gmail.com>
2022-08-22 19:43:11 +0700

    routing設定変更

---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScheduleController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        dd('s');
+    }
+}
