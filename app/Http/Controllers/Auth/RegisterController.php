a83078ae3aeae2a789449a832d255e7718b97ae6
wanosukecake <wanosukecake@gmail.com>
2022-08-30 17:12:37 +0700

    リリース時にマイグレーションが行われるよう変更

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd(User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]));
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
