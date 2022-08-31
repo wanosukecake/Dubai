d753a367db0a67feee903cf410b8c274278b0a53
wanosukecake <wanosukecake@gmail.com>
2022-08-31 21:32:04 +0700

    home Routeを仮でスケジュールページに設定、スケジュールページを認証ミドルウェア配下に設定

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ScheduleController extends Controller
 {
     public function __construct()
     {
-
+        parent::__construct();
     }
 
     /**
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/schedule';
 
     /**
      * The controller namespace for the application.
